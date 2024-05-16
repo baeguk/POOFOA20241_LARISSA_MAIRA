633b725dd68de1da9a3a8cb04cdf3686879cad9a
rosenclever <rosenclever@gmail.com>
2024-05-15 21:02:07 -0300

    construtores de conta, metodos de acesso a numero e saldo

--- SistemaFinanceiro/Program.cs
+++ SistemaFinanceiro/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using SistemaFinanceiro.Model;
+
+var conta1 = new Conta(123);
+Console.WriteLine($"Conta: {conta1.Numero}");
+//conta1.Numero = 321;
