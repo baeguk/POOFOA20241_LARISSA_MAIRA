5a1ee90ac4a1e596ca2d16dc82b1f592489afaa1
rosenclever <rosenclever@gmail.com>
2024-05-22 20:38:17 -0300

    criação da classe agencia

---
+++ SistemaFinanceiro/Agencia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaFinanceiro
+{
+    public class Agencia
+    {
+        private int _numero;
+        private string _nome, _telefone;
+
+        public Agencia(int numero)
+        {
+            _numero = numero;
+        }
+        public int Numero { get => _numero; }
+        public string Nome 
+        { 
+            get => _nome; 
+            set => _nome = value;
+        }
+        public string Telefone
+        {
+            get => _telefone;
+            set
+            {
+                _telefone = value;
+            }
+        }
+    }
+}
